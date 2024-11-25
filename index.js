eb2e9748e0dcff675b3c4f9d7856365bacb32b43
AlejandroPenaAllende <alejandro.pena.allende2304@gmail.com>
2024-11-24 22:08:04 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("data.json");
-const router2 = jsonServer.router("IMG");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 10000;
 
 server.use(middlewares);
 server.use(router);
-server.use(router2);
 
 server.listen(port);
